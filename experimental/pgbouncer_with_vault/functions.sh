7914a9ec271ddcc2fd17347c196a7dff69f0db1e
Stephen Stubbs <stephen.stubbs@vanqor.com>
2019-10-15 00:35:32 +0100

    cleanup

--- experimental/pgbouncer_with_vault/functions.sh
+++ experimental/pgbouncer_with_vault/functions.sh
@@ -3,7 +3,7 @@
 #=================================================================================================
 # REUSABLE FUNCTIONS
 #-------------------------------------------------------------------------------------------------
-json_array() {
+function json_array() {
   echo -n '['
   while [ $# -gt 0 ]; do
     x=${1//\\/\\\\}
@@ -14,19 +14,19 @@ json_array() {
   echo ']'
 }
 
-function replace_with_env(){
-    # replace any {{ENV_NAME}} with its respective env value.
-    value="$1"
+function replace_with_env() {
+  # replace any {{ENV_NAME}} with its respective env value.
+  value="$1"
 
-    # the regular expression matches {{ [SOME NAME] }}
-    re='(.*)\{\{\s*(\w+)\s*\}\}(.*)'
+  # the regular expression matches {{ [SOME NAME] }}
+  re='(.*)\{\{\s*(\w+)\s*\}\}(.*)'
 
-    # search for all replacements.
-    while [[ "$value" =~ $re ]]; do
-        env_name=${BASH_REMATCH[2]}
-        value="${BASH_REMATCH[1]}${!env_name}${BASH_REMATCH[3]}"
-    done
+  # search for all replacements.
+  while [[ "$value" =~ $re ]]; do
+    env_name=${BASH_REMATCH[2]}
+    value="${BASH_REMATCH[1]}${!env_name}${BASH_REMATCH[3]}"
+  done
 
-    echo "$value"
+  echo "$value"
 }
 #=================================================================================================
--- experimental/pgbouncer_with_vault/install.sh
+++ experimental/pgbouncer_with_vault/install.sh
@@ -39,8 +39,8 @@ helmfile sync || exit 1
 #-------------------------------------------------------------------------------------------------
 # Turns of affinity if one node cluster is set to true for testing purposes
 git clone --single-branch --branch v0.9.0 https://github.com/hashicorp/consul-helm.git
-if [ "$SINGLE_NODE_CLUSTER" = true ] ; then
-    sed -i '/affinity: |/,+8 s/^/#/' consul-helm/values.yaml
+if [ "$SINGLE_NODE_CLUSTER" = true ]; then
+  sed -i '/affinity: |/,+8 s/^/#/' consul-helm/values.yaml
 fi
 helm install "${CONSUL_NAME}" ./consul-helm
 rm -rf consul-helm
@@ -63,9 +63,9 @@ INIT_OUTPUT=$(kubectl exec -it "${VAULT_NAME}-0" -- vault operator init -n 1 -t 
 sleep 15
 
 UNSEAL_KEY=$(echo "${INIT_OUTPUT}" | grep 'Unseal Key 1:' | cut -d" " -f4)
-UNSEAL_KEY=$(sed 's/\x1b\[[0-9;]*m//g' <<< $UNSEAL_KEY) # remove ansi colour ^[[0m^M
+UNSEAL_KEY=$(sed 's/\x1b\[[0-9;]*m//g' <<<$UNSEAL_KEY) # remove ansi colour ^[[0m^M
 ROOT_TOKEN=$(echo "${INIT_OUTPUT}" | grep 'Initial Root Token:' | cut -d" " -f4)
-ROOT_TOKEN=$(sed 's/\x1b\[[0-9;]*m//g' <<< $ROOT_TOKEN) # remove ansi colour ^[[0m^M
+ROOT_TOKEN=$(sed 's/\x1b\[[0-9;]*m//g' <<<$ROOT_TOKEN) # remove ansi colour ^[[0m^M
 
 kubectl exec -it "${VAULT_NAME}-0" -- vault operator unseal "${UNSEAL_KEY}"
 kubectl exec -it "${VAULT_NAME}-1" -- vault operator unseal "${UNSEAL_KEY}"
@@ -81,25 +81,25 @@ kubectl exec -it "${VAULT_NAME}-0" -- vault login "${ROOT_TOKEN}"
 #-------------------------------------------------------------------------------------------------
 kubectl exec -it "${VAULT_NAME}-0" -- vault secrets enable database
 kubectl exec -it "${VAULT_NAME}-0" -- vault write database/config/postgres \
-    plugin_name=postgresql-database-plugin \
-    allowed_roles="connection-pool-role" \
-    connection_url="postgresql://{{username}}:{{password}}@${PGXL_SERVICE_NAME}:5432/postgres?sslmode=disable" \
-    username="postgres" \
-    password="${PASSWORD}"
+  plugin_name=postgresql-database-plugin \
+  allowed_roles="connection-pool-role" \
+  connection_url="postgresql://{{username}}:{{password}}@${PGXL_SERVICE_NAME}:5432/postgres?sslmode=disable" \
+  username="postgres" \
+  password="${PASSWORD}"
 
 CONNECTION_POOL_ROLE_CREATION=(
-"DO \$FUNC\$
+  "DO \$FUNC\$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'connection_pool') THEN
         CREATE ROLE connection_pool;
     END IF;
 END
 \$FUNC\$;"
-"CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}' IN ROLE connection_pool;"
+  "CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}' IN ROLE connection_pool;"
 )
 
 CONNECTION_POOL_PGSHADOW_LOOKUP_FUNCTION=(
-"DO \$FUNC1\$
+  "DO \$FUNC1\$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM information_schema.schemata WHERE schema_name = 'connection_pool') THEN
         CREATE SCHEMA connection_pool;
@@ -122,10 +122,10 @@ ROLE_AND_PG_SHADOW_LOOKUP=("${CONNECTION_POOL_ROLE_CREATION[@]}" "${CONNECTION_P
 ROLE_AND_PG_SHADOW_LOOKUP_JSON=$(json_array "${ROLE_AND_PG_SHADOW_LOOKUP[@]}")
 
 kubectl exec -it "${VAULT_NAME}-0" -- vault write database/roles/connection-pool-role \
-    db_name=postgres \
-    creation_statements="${ROLE_AND_PG_SHADOW_LOOKUP_JSON}" \
-    default_ttl="1h" \
-    max_ttl="24h"
+  db_name=postgres \
+  creation_statements="${ROLE_AND_PG_SHADOW_LOOKUP_JSON}" \
+  default_ttl="1h" \
+  max_ttl="24h"
 #=================================================================================================
 
 #=================================================================================================
@@ -144,20 +144,26 @@ vault policy write connection-pool-role-policy connection-pool-role-policy.hcl;"
 #-------------------------------------------------------------------------------------------------
 kubectl apply -f postgres-serviceaccount.yaml
 #-------------------------------------------------------------------------------------------------
-VAULT_SA_NAME=$(kubectl get sa postgres-vault -o jsonpath="{.secrets[*]['name']}"); \
-SA_JWT_TOKEN=$(kubectl get secret $VAULT_SA_NAME -o jsonpath="{.data.token}" | base64 --decode; echo); \
-SA_CA_CRT=$(kubectl get secret $VAULT_SA_NAME -o jsonpath="{.data['ca\.crt']}" | base64 --decode; echo); \
-K8S_HOST=$(kubectl exec consul-consul-server-0 -- sh -c 'echo $KUBERNETES_SERVICE_HOST'); \
-kubectl exec -it "${VAULT_NAME}-0" -- vault auth enable kubernetes; \
+VAULT_SA_NAME=$(kubectl get sa postgres-vault -o jsonpath="{.secrets[*]['name']}")
+SA_JWT_TOKEN=$(
+  kubectl get secret $VAULT_SA_NAME -o jsonpath="{.data.token}" | base64 --decode
+  echo
+)
+SA_CA_CRT=$(
+  kubectl get secret $VAULT_SA_NAME -o jsonpath="{.data['ca\.crt']}" | base64 --decode
+  echo
+)
+K8S_HOST=$(kubectl exec consul-consul-server-0 -- sh -c 'echo $KUBERNETES_SERVICE_HOST')
+kubectl exec -it "${VAULT_NAME}-0" -- vault auth enable kubernetes
 kubectl exec -it "${VAULT_NAME}-0" -- vault write auth/kubernetes/config \
   token_reviewer_jwt="$SA_JWT_TOKEN" \
   kubernetes_host="https://$K8S_HOST:443" \
-  kubernetes_ca_cert="$SA_CA_CRT"; \
+  kubernetes_ca_cert="$SA_CA_CRT"
 kubectl exec -it "${VAULT_NAME}-0" -- vault write auth/kubernetes/role/postgres \
-    bound_service_account_names=postgres-vault \
-    bound_service_account_namespaces=default \
-    policies=connection-pool-role-policy \
-    ttl=24h;
+  bound_service_account_names=postgres-vault \
+  bound_service_account_namespaces=default \
+  policies=connection-pool-role-policy \
+  ttl=24h
 #=================================================================================================
 
 #=================================================================================================
