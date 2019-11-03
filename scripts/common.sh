6c1bfcaa5914687e264b103ab19acf1221fe139f
Zav <zav@cherre.com>
2019-11-03 12:29:16 -0500

    Added color logging and assert methods.

---
+++ scripts/common.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+# black="\e[0;30m"
+# red="\e[0;31m"
+# green="\e[0;32m"
+# yellow="\e[0;33m"
+# blue="\e[0;34m"
+# purple="\e[0;35m"
+# cyan="\e[0;36m"
+# white="\e[0;37m"
+# orange="\e[0;91m"
+
+# Methods and functions to be used in all other scripts.
+: ${LOGGING_SHOW_COLORS:="true"}
+: ${LOGGING_INFO_COLOR:="\e[0;32m"}
+: ${LOGGING_INFO_PREFEX:=":INFO"}
+: ${LOGGING_ERROR_COLOR:="\e[0;31m"}
+: ${LOGGING_ERROR_PREFEX:=":ERROR"}
+: ${LOGGING_WARNING_COLOR:="\e[0;33m"}
+: ${LOGGING_WARNING_PREFEX:=":WARNING"}
+: ${LOGGING_SCRIPT_PREFEX:=":SCRIPT"}
+: ${LOGGING_SCRIPT_COLOR:="\e[0;35m"}
+
+# TODO: Apply common format for stackdriver.
+logging_core_print() {
+  local log_type="$1"
+  local reset_color="\e[39m"
+  # remove the first argument.
+  shift
+
+  local log_color_env_name="LOGGING_${log_type}_COLOR"
+  local log_type_prefex_env_name="LOGGING_${log_type}_PREFEX"
+  log_color="${!log_color_env_name}"
+  log_type_prefex="${!log_type_prefex_env_name}"
+
+  if [ "$LOGGING_SHOW_COLORS" != "true" ]; then
+    echo "$LOGGING_PREFIX$log_type_prefex " "$@"
+  else
+    echo -e "$log_color$LOGGING_PREFIX$log_type_prefex$reset_color" "$@"
+  fi
+}
+
+# log a line with the logging prefex.
+function log:info() {
+  logging_core_print "INFO" "$@"
+}
+
+function log:error() {
+  logging_core_print "ERROR" "$@"
+}
+
+function log:warning() {
+  logging_core_print "WARNING" "$@"
+}
+
+function log:script() {
+  logging_core_print "SCRIPT" "$@"
+}
+
+function assert() {
+  if [ "$1" -ne 0 ]; then
+    log:error "$2"
+    return "$1"
+  fi
+}
+
+function assert_warn() {
+  if [ "$1" -ne 0 ]; then
+    log:warning "$2"
+  fi
+}
